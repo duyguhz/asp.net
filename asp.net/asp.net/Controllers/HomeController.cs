11e63d267a653d9e8773f947539560403b9ce8f0
duyguhz <duyghuhh@code.edu.az>
2022-08-30 11:24:30 +0400

    hw

---
+++ asp.net/asp.net/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using asp.net.Models;
+
+namespace asp.net.Controllers
+{
+    public class HomeController : Controller
+    {
+       public IActionResult Index()
+        {
+            Student std1 = new Student { Name="Duygu" , Surname="Hesenzade" , Age=17};
+            Student std2 = new Student { Name = "Kemale", Surname = "Camalova", Age = 21 };
+            Student std3 = new Student { Name = "Xeyal", Surname = "Memmedzade", Age = 25 };
+            List<Student> students = new List<Student>();
+            students.Add(std1);
+            students.Add(std2);
+            students.Add(std3);
+
+            return View(students);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ asp.net/asp.net/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace asp.net.Models
+{
+    public class Student
+    {
+      public string Name { get; set; }
+      public string Surname { get; set; }
+      public int Age { get; set; }
+    }
+}
